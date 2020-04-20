7c22d34d9ab05c6da04f146e7b4680166b9910c4
Deep Panchal <deep302001@gmail.com>
2020-04-19 21:18:29 -0500

    v1.0 functioning as expected

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <template>
   <v-app>
-    <v-app-bar app color="green" light>
+    <v-app-bar app color="green">
       <div class="d-flex align-center">
         <h2>News Fetcher</h2>
       </div>
       <v-spacer></v-spacer>
       <v-btn-toggle color="white" mandatory group>
-        <v-btn value="light">Light</v-btn>
-        <v-btn v-on:click="themeDark" value="dark">Dark</v-btn>
+        <v-btn @click="themeLight" value="light">Light</v-btn>
+        <v-btn @click="themeDark" value="dark">Dark</v-btn>
       </v-btn-toggle>
     </v-app-bar>
 
     <v-content>
       <v-container>
+        <!-- Rendering buttons for all news categories -->
         <v-text-field label="Search..." single-line outlined></v-text-field>
-          <v-btn-toggle v-for="topic in newsTopics" :key="topic" color="white" mandatory group>
-            <v-btn @click="fetchData(topic)">{{topic}}</v-btn>
+        <div>
+          <v-btn-toggle class="d-flex justify-center align-center" text mandatory group>
+            <v-btn v-for="topic in newsTopics" :key="topic" @click="fetchData(topic)">{{topic}}</v-btn>
           </v-btn-toggle>
+        </div>
+
+        <!-- Render Article Cards -->
+        <v-card
+          v-for="article in articles.results"
+          :key="article"
+          class="mx-auto my-4"
+          max-width="400"
+          hover
+          :href="article.url"
+          target="_blank"
+        >
+          <v-img class="white--text align-end" height="230px" :src="article.multimedia[0].url">
+            <v-chip class="my-2 mx-2">{{article.section}}</v-chip>
+          </v-img>
+          <v-card-title>{{article.title}}</v-card-title>
+          <v-card-subtitle>{{article.byline}}</v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>{{article.abstract}}</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="green" :href="article.url" target="_blank" text block>
+              View article
+              <v-icon right>mdi-open-in-new</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        <!-- ----------------------------------------------------------------------- -->
       </v-container>
     </v-content>
   </v-app>
@@ -30,8 +62,8 @@ export default {
 
   data: () => ({
     apiKey: process.env.VUE_APP_APIKEY,
-    url: `https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${process.env.VUE_APP_APIKEY}`,
-    appTheme: "",
+    url: "",
+    articles: "",
     newsTopics: [
       "arts",
       "automobiles",
@@ -51,19 +83,17 @@ export default {
     ]
   }),
   methods: {
-    themeDark() {
-      this.$Vuetify.theme.dark = true;
-    },
+    themeLight() {this.$vuetify.theme.dark = false},
+    themeDark() {this.$vuetify.theme.dark = true},
     fetchData(topic) {
       console.log(topic);
       let url = `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${this.apiKey}`;
-      this.$axios.get(url)
-            .then(response => {
-              let data = response.data;
-              console.log(url);
-              console.log(data.results[0]);
-            })
-    },
-  }
+      this.$axios.get(url).then(response => {
+        let data = response.data;
+        console.log(data.results);
+        this.articles = data;
+      });
+    }
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
-import vuetify from './plugins/vuetify';
+import Vuetify from 'vuetify/lib';
 import App from './App.vue'
 const axios = require('axios');
-Vue.prototype.$axios = axios
-Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+Vue.use(Vuetify);
+Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
+  el: '#app',
+  vuetify: new Vuetify({
+    theme: {
+      dark: false
+    }
+  }),
   render: h => h(App)
-}).$mount('#app')
+})
