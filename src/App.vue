49d620e1759f441bfb6a85f816aaba1f9cfc7155
Deep Panchal <deep302001@gmail.com>
2020-04-22 22:07:15 -0500

    major update (add appbar, change colors)

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,75 @@
 <template>
   <v-app>
-    <v-app-bar app color="green">
-      <div class="d-flex align-center">
-        <h2>News Fetcher</h2>
-      </div>
+    <!-- Navigation menu -->
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <v-list dense nav>
+        <v-list-item-group active-class="light-blue--text text--accent-4">
+          <v-list-item v-for="(topic, index) in newsTopics" :key="index" link>
+            <v-list-item-content @click="loading=true;fetchData(topic);drawer=false;">
+              <v-list-item-title>{{topic.toUpperCase()}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="amber" :clipped-left="$vuetify.breakpoint.lgAndUp">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">NyTimes News Fetcher</span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      />
       <v-spacer></v-spacer>
-      <v-btn-toggle color="white" mandatory group>
-        <v-btn @click="themeLight" value="light">Light</v-btn>
-        <v-btn @click="themeDark" value="dark">Dark</v-btn>
+      <v-btn-toggle mandatory group>
+        <v-btn icon @click="themeLight">
+          <v-icon>mdi-white-balance-sunny</v-icon>
+        </v-btn>
+        <v-btn icon @click="themeDark">
+          <v-icon>mdi-brightness-2</v-icon>
+        </v-btn>
       </v-btn-toggle>
+      <v-progress-linear
+        :active="loading"
+        :indeterminate="loading"
+        absolute
+        bottom
+        color="light-blue"
+      ></v-progress-linear>
     </v-app-bar>
 
     <v-content>
       <v-container>
-        <!-- Rendering buttons for all news categories -->
-        <!-- <v-text-field label="Search..." single-line outlined></v-text-field> -->
-        <v-btn
-          class="col-4 my-1 deep-orange--text"
-          @click="fetchData(topic)"
-          v-for="topic in newsTopics"
-          :key="topic"
-          text
-          elevation="1"
-        >{{topic}}</v-btn>
-
         <!-- Render Article Cards -->
         <v-card
-          v-for="article in articles.results"
-          :key="article"
+          v-for="(article, index) in articles"
+          :key="index"
           class="mx-auto my-4"
           max-width="400"
           hover
-          :href="article.url"
-          target="_blank"
         >
-          <v-img class="white--text align-end" height="230px" :src="article.multimedia[0].url">
-            <v-chip class="my-2 mx-2">{{article.section}}</v-chip>
+          <v-img
+            class="white--text align-end"
+            height="230px"
+            :src="article['multimedia'][0]['url']"
+          >
+            <v-chip class="my-2 mx-2">{{article.section.toUpperCase()}}</v-chip>
           </v-img>
           <v-card-title>{{article.title}}</v-card-title>
           <v-card-subtitle>{{article.byline}}</v-card-subtitle>
 
-          <v-card-text class="text--primary">
+          <v-card-text>
             <div>{{article.abstract}}</div>
           </v-card-text>
 
           <v-card-actions>
-            <v-btn color="green" :href="article.url" target="_blank" text block>
+            <v-btn color="light-blue text--accent-4" :href="article.url" target="_blank" text block>
               View article
               <v-icon right>mdi-open-in-new</v-icon>
             </v-btn>
@@ -64,12 +88,11 @@ export default {
   components: {},
 
   data: () => ({
-    apiKey: process.env.VUE_APP_APIKEY,
-    url: "",
+    drawer: null,
+    loading: false,
     articles: "",
     newsTopics: [
       "arts",
-      "automobiles",
       "business",
       "fashion",
       "food",
@@ -80,6 +103,7 @@ export default {
       "realestate",
       "science",
       "sports",
+      "insider",
       "technology",
       "travel",
       "world"
@@ -94,13 +118,18 @@ export default {
     },
     fetchData(topic) {
       console.log(topic);
-      let url = `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${this.apiKey}`;
+      let url = `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${process.env.VUE_APP_APIKEY}`;
       this.$axios.get(url).then(response => {
-        let data = response.data;
-        console.log(data.results);
-        this.articles = data;
+        this.articles = response.data.results;
+        console.log(this.articles[0]["multimedia"][0]["url"]);
+        console.log(this.articles[0]["title"]);
+        console.log(this.articles[0]["byline"]);
+        console.log(this.articles[0]["url"]);
+        console.log(this.articles[0]["section"]);
+        console.log(this.articles[0]["abstract"]);
+        this.loading = false;
       });
     }
-  }
+  },
 };
 </script>
