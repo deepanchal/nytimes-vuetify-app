8103186818fdced7df46412beb7719b78360fc6a
Deep Panchal <deep302001@gmail.com>
2020-04-23 18:33:11 -0500

    deploy

---
+++ precache-manifest.46780799db311c6a811d8d858e84415d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4557c3f89ebe9ecc69",
+    "url": "/nytimes-vuetify-app/css/chunk-vendors.338e0e48.css"
+  },
+  {
+    "revision": "e227e4013fc842c8ca9d5db493dda844",
+    "url": "/nytimes-vuetify-app/index.html"
+  },
+  {
+    "revision": "8bb8a260b3bc169f1a9b",
+    "url": "/nytimes-vuetify-app/js/app.c6af8481.js"
+  },
+  {
+    "revision": "fe4557c3f89ebe9ecc69",
+    "url": "/nytimes-vuetify-app/js/chunk-vendors.99bba2dd.js"
+  },
+  {
+    "revision": "09442b77d5597ffdaba273ce2dc8ac18",
+    "url": "/nytimes-vuetify-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nytimes-vuetify-app/robots.txt"
+  }
+]);
